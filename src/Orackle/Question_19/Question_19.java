896c33c21bb2cab6a35067103294f3486e31d837
Gulsen1 <60560578+Gulsen1@users.noreply.github.com>
2020-08-15 09:42:27 -0500

    MyFirst Orcale Commit

---
+++ src/Orackle/Question_19/Question_19.java
@@ -0,0 +1,9 @@
+package Orackle.Question_19;
+
+public class Question_19 {
+    int p;
+    private int q;
+    protected int r;
+    public int s;
+    //Same class extendable different class, you can see p,r,s
+}
